4e8503138b72fbd0fa0826c4e49cedfb712e4941
Niki Namian <nikinamian@gmail.com>
2024-11-20 17:30:21 -0800

    Initial Commit

---
+++ Smallest Number Calculator/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Smallest Number Calculator
+//
+//  Created by Niki Namian on 11/20/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
